7512207de1add0b013d0a1d249dabf96b8ada847
Allenkayode <64140701+Allenkayode@users.noreply.github.com>
2023-09-14 22:00:19 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const Config = require("./utils/config");
+const { initMongo } = require("./config/db");
+const { router } = require("./router");
+
+initMongo();
+
+app.use(express.json());
+app.use(
+  expressSession({
+    secret: "Rusty is a dog",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(router);
+
+const app = express();
+
+app.listen(Config.Port, () => {
+  console.log(`api running on ${Config.Port}`);
+});
