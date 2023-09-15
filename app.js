a31c349c016fed6e9a33f4fd8c414a4710dfdf3b
Allenkayode <64140701+Allenkayode@users.noreply.github.com>
2023-09-15 17:21:36 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const app = express();
 const Config = require("./utils/config");
 const { initMongo } = require("./config/db");
 const { route } = require("./routes/PersonRoute");
 
 initMongo();
 
-const app = express();
+
 
 app.use(express.json());
 app.use(
