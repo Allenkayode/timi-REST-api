e7b62dbfdc805e67bf1c7158d2c1c742b08b3771
Allenkayode <64140701+Allenkayode@users.noreply.github.com>
2023-09-16 12:47:52 +0100

    Create Server.js

---
+++ Server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const Config = require("./utils/config");
+const { initMongo } = require("./config/db");
+const { route } = require("./routes/PersonRoute");
+
+initMongo();
+app.use(express.json());
+
+app.use("/", route);
+
+
+
+app.listen(Config.Port, () => {
+  console.log(`api running on ${Config.Port}`);
+});
